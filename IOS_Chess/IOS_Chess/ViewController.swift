9460b145b536ce4081ee180c666532267b93f237
rjmorley <rjmorley@me.com>
2019-04-04 16:16:10 -0600

    first push

---
+++ IOS_Chess/IOS_Chess/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS_Chess
+//
+//  Created by rj morley on 4/4/19.
+//  Copyright © 2019 ___rickjames___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
